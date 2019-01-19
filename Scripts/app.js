f946c146b283f08f3cffdc792541d5d9c0ee43b3
Rajan Dhaliwal <rajandhaliwal80@gmail.com>
2019-01-19 16:27:31 -0500

    some changes in index.html

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,12 +3,9 @@
 // IIFE -- Immediately Invoked Function Expression
 (function(){
 
-    let myFunctionalVariable = 0;
-
 function Start(){
     let myLocalVariable = 0;
-    console.log(`App Started...${myFunctionalVariable}`);
+    console.log(`%c App Started...`, "font-size:20 px; color:blue;");
 }
-
 window.addEventListener("load", Start);
 })();
