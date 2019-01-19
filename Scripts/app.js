b265d4adfc6b0b2c1ee32a2d44e35c3404e1724d
Rajan Dhaliwal <rajandhaliwal80@gmail.com>
2019-01-19 14:47:36 -0500

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+"use strict";
+//self-executing anonymous funstion
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    let myFunctionalVariable = 0;
+
+function Start(){
+    let myLocalVariable = 0;
+    console.log(`App Started...${myFunctionalVariable}`);
+}
+
+window.addEventListener("load", Start);
+})();
